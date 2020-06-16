8fc8524e9732ac106fdb5ad84eed6dfba741b324
unknown <davidgun13@ru.is>
2020-06-16 02:24:58 +0000

    experementing with bootstrap

--- myproject/urls.py
+++ myproject/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
+    path('homepage/', include('homepage.urls')),
     path('admin/', admin.site.urls),
 ]
