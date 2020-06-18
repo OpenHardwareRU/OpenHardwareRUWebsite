d07f98a69da6ffde9f8a91da9745881c13cbb402
unknown <davidgun13@ru.is>
2020-06-18 04:05:34 +0000

    carousel finished, added about page, and just geting to know django, still messing around

---
+++ homepage/views.py
@@ -0,0 +1,21 @@
+
+# Create your views here.
+
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+
+from .models import Choice, Question
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'homepage/index.html', context)
+
+def about(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'homepage/about.html', context)
+
+def shop(request):
+    return render(request, 'homepage/shop.html')
